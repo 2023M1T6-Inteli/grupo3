c25162b6851a71b3e0766b87dff607e52c4fc282
Gustavo674 <gustavo.gouveia@sou.inteli.edu.br>
2023-02-15 13:58:32 -0300

    Botao em controles

---
+++ Godot/UniWay/Sair.gd
@@ -0,0 +1,14 @@
+extends Button
+
+
+
+func _ready():
+	pass 
+
+
+func _on_Sair_tree_exiting():
+	get_tree().change_scene("res://Control.tscn")
+	pass
+
+func _on_Sair_pressed():
+	pass ;get_tree().change_scene("res://Control.tscn")
