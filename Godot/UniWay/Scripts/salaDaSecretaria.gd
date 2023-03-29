7a07d661e95e5d84933d8f9ba0048562b1f5403d
marcelaissa <marcela.martins@sou.inteli.edu.br>
2023-03-29 15:57:39 -0300

    Adicionei personagem

--- Godot/UniWay/Scripts/salaDaSecretaria.gd
+++ Godot/UniWay/Scripts/salaDaSecretaria.gd
@@ -5,6 +5,7 @@ func _enter_tree():
 	$caminhoneiro1/Light2D.visible = false
 	$caminhoneiro2/Light2D.visible = false
 	$Caminhoneira/Light2D.visible = false
+	$Caminhoneira2/Light2D.visible = false
 
 #função que altera os textos de acordo com o idioma escolhido
 func _process(delta):
@@ -30,6 +31,7 @@ func _on_Caminhoneiro1_pressed():
 	$Caminhoneira.visible = false
 	$caminhoneiro1.visible = false
 	$caminhoneiro2.visible = false
+	$Caminhoneira2.visible = false
 	$selecionarPersonagem.visible = false
 	$caminhoneiro1Selecionado.visible = true
 	$SecretariaSentada.visible = true
@@ -51,6 +53,7 @@ func _on_caminhoneiro2_pressed():
 	$Caminhoneira.visible = false
 	$caminhoneiro1.visible = false
 	$caminhoneiro2.visible = false
+	$Caminhoneira2.visible = false
 	$selecionarPersonagem.visible = false
 	$caminhoneiro2Selecionado.visible = true
 	$SecretariaSentada.visible = true
@@ -72,10 +75,33 @@ func _on_Caminhoneira_pressed():
 	$Caminhoneira.visible = false
 	$caminhoneiro1.visible = false
 	$caminhoneiro2.visible = false
+	$Caminhoneira2.visible = false
 	$selecionarPersonagem.visible = false
 	$caminhoneiraSelecionada.visible = true
 	$SecretariaSentada.visible = true
 	Global.personagemSelecionado = 2
+	yield(get_tree().create_timer(1), "timeout")
+	alteradorDeIdioma()
+	
+#função que faz o efeito de luz aparecer 
+func _on_Caminhoneira2_mouse_entered():
+	$Caminhoneira2/Light2D.visible = true 
+
+#função que faz o efeito de luz desaparecer
+func _on_Caminhoneira2_mouse_exited():
+	$Caminhoneira2/Light2D.visible = false
+	
+#função que continua o jogo de acordo com o personagem escolhido
+func _on_Caminhoneira2_pressed():
+	$ColorRect/AnimationPlayer.play("voltandoAoNormal")
+	$Caminhoneira.visible = false
+	$caminhoneiro1.visible = false
+	$caminhoneiro2.visible = false
+	$Caminhoneira2.visible = false
+	$selecionarPersonagem.visible = false
+	$Caminhoneira2Selecionado.visible = true
+	$SecretariaSentada.visible = true
+	Global.personagemSelecionado = 0
 	yield(get_tree().create_timer(1), "timeout")
 	alteradorDeIdioma()
 	
